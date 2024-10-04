9a12937bbd5f2abe441edc618827f41eec05d042
nanacnote <adjeibohyen@hotmail.co.uk>
2024-10-04 12:37:49 +0100

    refactor: update app configs

--- src/libs/config.ts
+++ src/libs/config.ts
@@ -1,3 +1,9 @@
+export const COOKIE_KEY_USER_STATIC_TOKEN = "user-static-token"
+
+// do not import default int <script> as this may leak private details.
+// Use corresponding name exports above
 export default {
-    URL_QUERY_PARAM_ID: "id"
+    URL_QUERY_PARAM_ID: "id",
+    COOKIE_KEY_USER_STATIC_TOKEN
 }
+
