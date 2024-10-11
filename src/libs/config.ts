9cddb3d8fbaad99b1a186a33bc487a735728547d
nanacnote <adjeibohyen@hotmail.co.uk>
2024-10-11 01:13:03 +0100

    refactor: add directus file assets prefix url

--- src/libs/config.ts
+++ src/libs/config.ts
@@ -1,9 +1,8 @@
-export const COOKIE_KEY_USER_STATIC_TOKEN = "user-static-token"
+export const COOKIE_KEY_USER_STATIC_TOKEN = "user-static-token";
 
-// do not import default int <script> as this may leak private details.
-// Use corresponding name exports above
+// do not import default values below into a <script> tag as this may leak private details. Use corresponding name exports above
 export default {
-    URL_QUERY_PARAM_ID: "id",
-    COOKIE_KEY_USER_STATIC_TOKEN
-}
-
+  DIRECTUS_FILE_ASSET_URL_PREFIX: `${import.meta.env.DIRECTUS_SITE_URL}/assets/`,
+  URL_QUERY_PARAM_ID: "id",
+  COOKIE_KEY_USER_STATIC_TOKEN,
+};
