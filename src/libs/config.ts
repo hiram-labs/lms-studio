33003def837422e31eaac743b4b8d1194de6582c
nanacnote <adjeibohyen@hotmail.co.uk>
2024-10-13 20:26:10 +0100

    refactor: add logic for apk start heuristic

--- src/libs/config.ts
+++ src/libs/config.ts
@@ -1,8 +1,9 @@
 export const COOKIE_KEY_USER_STATIC_TOKEN = "user-static-token";
+export const DIRECTUS_FILE_ASSET_URL_PREFIX = `${import.meta.env.PUBLIC_DIRECTUS_SITE_URL}/assets/`;
 
 // do not import default values below into a <script> tag as this may leak private details. Use corresponding name exports above
 export default {
-  DIRECTUS_FILE_ASSET_URL_PREFIX: `${import.meta.env.DIRECTUS_SITE_URL}/assets/`,
   URL_QUERY_PARAM_ID: "id",
+  DIRECTUS_FILE_ASSET_URL_PREFIX,
   COOKIE_KEY_USER_STATIC_TOKEN,
 };
