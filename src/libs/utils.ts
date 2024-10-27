9daf2f83563e7525f853902d754705dae1ee15dd
nanacnote <adjeibohyen@hotmail.co.uk>
2024-10-27 20:29:56 +0000

    refactor: pass user token to deeplink

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -1,21 +1,27 @@
 export function getRandomSample<T>(arr: T[], numItems: number) {
-    // Shuffle the array using Fisher-Yates algorithm
-    for (let i = arr.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [arr[i], arr[j]] = [arr[j], arr[i]];
-    }
-    return arr.slice(0, numItems);
+  // Shuffle the array using Fisher-Yates algorithm
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr.slice(0, numItems);
 }
 
 export function generateDashboardBreadcrumbs(url: URL) {
-    const segments = url.pathname.split('/').filter(Boolean);
-    const query = url.search;
-    return segments.map((segment, i) => ({
-        name: segment.charAt(0).toUpperCase() + segment.slice(1),
-        url: `/${segments.slice(0, i + 1).join('/')}${query && i === segments.length - 1 ? query : ''}`,
-    }))
+  const segments = url.pathname.split("/").filter(Boolean);
+  const query = url.search;
+  return segments.map((segment, i) => ({
+    name: segment.charAt(0).toUpperCase() + segment.slice(1),
+    url: `/${segments.slice(0, i + 1).join("/")}${query && i === segments.length - 1 ? query : ""}`,
+  }));
 }
 
-export function getQueryParam(url: URL ,key: string) {
-    return new URL(url).searchParams.get(key);
+export function getQueryParam(url: URL, key: string) {
+  return new URL(url).searchParams.get(key);
+}
+
+export function getCookie(name: string) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop()!.split(";").shift();
 }
