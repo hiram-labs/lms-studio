1884cff5d154be4737a137be99d533ab717ab5a3
nanacnote <adjeibohyen@hotmail.co.uk>
2024-11-09 19:58:11 +0000

    chore: add lrs site url to config

--- src/libs/config.ts
+++ src/libs/config.ts
@@ -1,9 +1,15 @@
 export const COOKIE_KEY_USER_STATIC_TOKEN = "user-static-token";
-export const DIRECTUS_FILE_ASSET_URL_PREFIX = `${import.meta.env.PUBLIC_DIRECTUS_SITE_URL}/assets/`;
+export const LRS_SITE_URL = import.meta.env.PUBLIC_LRS_SITE_URL;
+export const LRS_XAPI_ENDPOINT = `${LRS_SITE_URL}/data/xAPI`;
+export const DIRECTUS_SITE_URL = import.meta.env.PUBLIC_DIRECTUS_SITE_URL;
+export const DIRECTUS_FILE_ASSET_URL_PREFIX = `${DIRECTUS_SITE_URL}/assets/`;
 
 // do not import default values below into a <script> tag as this may leak private details. Use corresponding name exports above
 export default {
   URL_QUERY_PARAM_ID: "id",
+  LRS_SITE_URL,
+  LRS_XAPI_ENDPOINT,
+  DIRECTUS_SITE_URL,
   DIRECTUS_FILE_ASSET_URL_PREFIX,
   COOKIE_KEY_USER_STATIC_TOKEN,
 };
