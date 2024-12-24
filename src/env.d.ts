2c09c5bc286291c2586064c23999c3feb261300e
nanacnote <adjeibohyen@hotmail.co.uk>
2024-12-24 04:30:42 +0000

    feat: add module download and installation android webviews

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,24 @@
 /// <reference path="../.astro/types.d.ts" />
+
+declare const Android: {
+  onModuleStartTrigger(uri: string): void;
+  onRestartAppTrigger(): void;
+
+  onUnhandledException(e: string): void;
+
+  onModuleDownloadStartTrigger(): void;
+  onSetModuleDownloadDetails(moduleName: string, fileSize: string): void;
+  onModuleDownloadProgress(val: number): void;
+  onModuleDownloadComplete(): void;
+  onModuleDownloadSuccess(): void;
+  onModuleDownloadFail(): void;
+
+  onModuleInstallStartTrigger(): void;
+  onModuleInstallRetryTrigger(): void;
+  onModuleInstallComplete(): void;
+  onModuleInstallSuccess(): void;
+  onModuleInstallFail(): void;
+
+  onModuleLaunchStartTrigger(): void;
+  onModuleLaunchFail(): void;
+};
