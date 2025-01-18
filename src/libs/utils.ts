1d8d3a4e3ec82df327806c0f81598fb99b83cd56
nanacnote <adjeibohyen@hotmail.co.uk>
2025-01-18 12:48:40 +0000

    refactor: improve cookie util function names

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -29,10 +29,16 @@ export function getQueryParam(url: URL, key: string) {
   return new URL(url).searchParams.get(key);
 }
 
-export function getCookie(name: string) {
+export function getCookieFromDocument(name: string) {
   const value = `; ${document.cookie}`;
   const parts = value.split(`; ${name}=`);
   if (parts.length === 2) return parts.pop()!.split(";").shift();
+}
+
+export function getCookieFromRequest(request: Request, name: string) {
+  const cookies = `; ${request.headers.get("cookie")}`;
+  const parts = cookies.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop()?.split(";").shift();
 }
 
 export function wrapPromiseWithTimeout<T>(
