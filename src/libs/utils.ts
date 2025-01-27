f2deb825061fba0ca1c5abb53282fc256b9439c2
nanacnote <adjeibohyen@hotmail.co.uk>
2025-01-27 09:30:34 +0000

    feat: add global alert component

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -1,4 +1,5 @@
 import type { ReadableStream as ReadableStreamType } from "@yume-chan/stream-extra/esm/types";
+import Toastify from "toastify-js";
 
 export function truncateText(text: string, maxChars: number): string {
   if (text.length > maxChars) {
@@ -138,4 +139,22 @@ export async function fetchApkFileWithProgress(
 
 export function delay(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+export function showAlert(
+  type: "info" | "success" | "warn" | "error",
+  message: string
+) {
+  const alerts = {
+    info: `<div role="alert" class="alert alert-info"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg><span>${message}</span></div>`,
+    success: `<div role="alert" class="alert alert-success"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg><span>${message}</span></div>`,
+    warn: `<div role="alert" class="alert alert-warning"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg><span>${message}</span></div>`,
+    error: `<div role="alert" class="alert alert-error"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg><span>${message}</span></div>`,
+  };
+  const node = document.createElement("div");
+  node.innerHTML = alerts[type];
+  Toastify({
+    node,
+    duration: 3500,
+  }).showToast();
 }
