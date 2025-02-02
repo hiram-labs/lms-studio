2afd96ea14d47d83d9d0efde87c8bae1aefba6a7
nanacnote <adjeibohyen@hotmail.co.uk>
2025-02-02 13:53:41 +0000

    refactor: localise org selection to page to conform with astro cookie setting

--- src/libs/directus.ts
+++ src/libs/directus.ts
@@ -1,4 +1,12 @@
-import { createDirectus, staticToken, rest } from "@directus/sdk";
+import {
+  createDirectus,
+  staticToken,
+  rest,
+  withToken,
+  readItems,
+  readMe,
+} from "@directus/sdk";
+import C from "./config";
 
 const TOKEN = import.meta.env.DIRECTUS_ACCESS_TOKEN;
 const PROTOCOL = import.meta.env.DIRECTUS_API_PROTOCOL || "http";
@@ -12,3 +20,33 @@ export const directusClientAuth = createDirectus(directusUrl)
   .with(rest());
 
 export const directusClientNoAuth = createDirectus(directusUrl).with(rest());
+
+export const fetchAccessibleOrganizations = async (
+  token: string,
+  role: string
+) => {
+  let orgResponses;
+  if (C.ACCESS_ALL_ORGS_ROLES.includes(role)) {
+    orgResponses = await directusClientNoAuth.request(
+      withToken(
+        token,
+        readItems("lms_organization", { fields: ["id", "organization_name"] })
+      )
+    );
+  } else {
+    const userId = await directusClientNoAuth.request(
+      withToken(token, readMe({ fields: ["id"] }))
+    );
+    orgResponses = await directusClientNoAuth.request(
+      withToken(
+        token,
+        readItems("lms_actor_access", {
+          filter: { user_id: userId },
+          fields: ["organization_id.id", "organization_id.organization_name"],
+        })
+      )
+    );
+    orgResponses = orgResponses.map((resp) => resp.organization_id);
+  }
+  return orgResponses;
+};
