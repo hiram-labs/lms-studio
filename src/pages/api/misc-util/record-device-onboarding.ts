cbe4efc0fd2d64eaa94c922c95b9e3cc862bf225
nanacnote <adjeibohyen@hotmail.co.uk>
2026-01-06 22:48:15 +0000

    refactor: fix record api call

--- src/pages/api/misc-util/record-device-onboarding.ts
+++ src/pages/api/misc-util/record-device-onboarding.ts
@@ -1,17 +1,11 @@
 import { directusClientNoAuth } from "../../../libs/directus";
-import C from "../../../libs/config";
 
-import { logger } from "@it-astro:logger";
 import { createItem, readMe, withToken } from "@directus/sdk";
 
 export default async (
   cookie: TApiReqCookie,
   data: TMiscUtilReqPayloadData["record-device-onboarding"]
 ): Promise<string> => {
-  // Get user ID from the token
-  const { id: userId } = await directusClientNoAuth.request(
-    withToken(cookie.TOKEN, readMe({ fields: ["id"] }))
-  );
 
   const userResponse = await directusClientNoAuth.request(
     withToken(cookie.TOKEN, createItem("lms_device", {
@@ -21,8 +15,6 @@ export default async (
       organization_id: cookie.ORG
     }))
   );
-
-  logger.info(`Device ${data.device_serial} onboarding started by user ${userId} in org ${cookie.ORG}`);
 
   return JSON.stringify({ success: true, device_id: userResponse.id });
 };
